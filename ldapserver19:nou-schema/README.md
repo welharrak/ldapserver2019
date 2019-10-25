3b04c7d6c161704f89390c5d6f38d06509eca020
Walid el Harrak <harrak31@gmail.com>
2019-10-25 10:43:14 +0200

    Nou schema

---
+++ ldapserver19:nou-schema/README.md
@@ -0,0 +1,10 @@
+# ldapserver19:base
+## ASIX M06-ASO @welharrak 18/20
+
+- Servidor bàsic ldap, amb organització dc=edt,dc=org,
+
+- Usuaris bàsics Pere, Marta, Anna, ...
+
+- Configuració client ldap.conf: ldapserver / dc=edt,dc=org
+
+- Schema futbolistes afegit, amb un nou objectclass i nous atributs
---
+++ ldapserver19:nou-schema/install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+# Install ldap server
+#________________________________
+rm -rf /etc/openldap/slapd.d/*
+rm -rf /var/lib/ldap/*
+
+mkdir /var/lib/ldap.edt.org
+
+cp /opt/docker/DB_CONFIG /var/lib/ldap
+
+slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
+
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.org.ldif
+
+chown -R ldap.ldap /etc/openldap/slapd.d
+chown -R ldap.ldap /var/lib/ldap
+
+
+cp /opt/docker/ldap.conf /etc/openldap/.
+
